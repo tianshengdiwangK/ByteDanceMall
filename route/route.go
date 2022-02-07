6daed64292e0ffe2aa2290793dba205865866b0e
leeg4ng <leeg4ng@qq.com>
2022-02-07 16:20:49 +0800

    完成3.3.2分类列表显示接口

--- route/route.go
+++ route/route.go
@@ -5,12 +5,14 @@ import (
 	"login_register_demo/controller/user"
 )
 
-func Init_route()  {
+func Init_route() {
 	router := gin.Default()
 	userOp := router.Group("/user")
 	{
 		userOp.GET("/login", user.UserLoginG)
 		userOp.GET("/register", user.UserRegisterG)
+
+		userOp.GET("/classification", user.GetMallCategory)
 	}
 	router.Run(":8080")
 }
