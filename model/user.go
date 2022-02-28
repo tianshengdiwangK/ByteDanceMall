b514f9fb60cdb3c19a60a008f98c63522a40ea8c
chenalian <1020484242@qq.com>
2022-02-28 17:16:15 +0800

    登录验证器

--- model/user.go
+++ model/user.go
@@ -10,8 +10,8 @@ type User struct {
 	UserPhone string `xorm:"not null comment('用户注册时的手机号') VARCHAR(255)"`
 	UserUsername string `xorm:"not null comment('用户名') VARCHAR(255)"`
 	UserPassword string `xorm:"not null comment('用户密码') VARCHAR(255)"`
-	Status       int    `xorm:"not null comment('记录用户是否登录') INT(11)"`
 	CreateTime time.Time `xorm:"not null comment('记录用户创建时间') DATETIME"`
 	Icon string `xorm:"not null comment('用户头像') VARCHAR(255)"`
+	GroupId       int    `xorm:"not null comment('记录用户类型，1表示admin，2表示普通用户') INT(11)"`
 }
 
