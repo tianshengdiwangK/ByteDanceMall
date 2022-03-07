6d5478d996dd4edda08a1ced2544b927223a2481
wugang <2865714246@qq.com>
2022-03-07 22:17:33 +0800

    新增获取分类列表接口

--- route/route.go
+++ route/route.go
@@ -1,9 +1,9 @@
 package route
 
 import (
-	"login_register_demo/middleware"
 	"login_register_demo/controller/shopAdmin"
 	"login_register_demo/controller/user"
+	"login_register_demo/middleware"
 	settings "login_register_demo/utils/setting"
 
 	"github.com/gin-gonic/gin"
@@ -23,17 +23,15 @@ func Init_route() {
 
 	}
 
-	shopAdminOp := router.Group("/shop/admin",middleware.CheckAdminAuth()) //商家后台操作，普通用户不可操作，需要权限认证是否为商家
-
+	shopAdminOp := router.Group("/shop/admin", middleware.CheckAdminAuth()) //商家后台操作，普通用户不可操作，需要权限认证是否为商家
 	{
 		shopAdminOp.POST("/product/image/add", shopAdmin.UploadProductImage)
 		shopAdminOp.POST("/product/add", shopAdmin.AddProduct)
 		shopAdminOp.POST("/product/sku/add", shopAdmin.ProductAddSku)
 		shopAdminOp.GET("/attribute_category", shopAdmin.GetAttributeCategory)
 		shopAdminOp.GET("/product/list", shopAdmin.GetGoodsList)
+		shopAdminOp.GET("/goods_category", shopAdmin.GetCategoryList)
 	}
-
-	
 
 	router.Run(settings.ServerSetting.HttpPort)
 
