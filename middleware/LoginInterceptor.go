70a072c730a94215c380d04b423cddaebdbc2c61
leeg4ng <leeg4ng@qq.com>
2022-03-10 17:21:10 +0800

    jwt增加user_id字段

--- middleware/LoginInterceptor.go
+++ middleware/LoginInterceptor.go
@@ -14,46 +14,47 @@ var JwtKey = []byte(config.Jwtkey)
 
 type MyClaims struct {
 	Username string `json:"username"`
+	UserId   int    `json:"user_id"`
 	jwt.StandardClaims
 }
+
 // 生成token
-func SetToken(username string) (string,int) {
-	expireTime := time.Now().Add(10 * time.Hour)	//过期时间10小时
+func SetToken(username string, userId int) (string, int) {
+	expireTime := time.Now().Add(10 * time.Hour) //过期时间10小时
 	setClaim := MyClaims{
 		Username: username,
+		UserId:   userId,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expireTime.Unix(),	//过期时间（时间戳）
-			Issuer: "ginblog",	//发行者
+			ExpiresAt: expireTime.Unix(), //过期时间（时间戳）
+			Issuer:    "ginblog",         //发行者
 		},
 	}
 
-	reqClaim := jwt.NewWithClaims(jwt.SigningMethodHS256,setClaim) //生成token
-	token,err := reqClaim.SignedString(JwtKey)                     //转换为字符串
-
-
+	reqClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, setClaim) //生成token
+	token, err := reqClaim.SignedString(JwtKey)                     //转换为字符串
 
 	if err != nil {
-		return "",403
+		return "", 403
 	}
-	return token,200
+	return token, 200
 }
+
 // 验证token
-func CheckToken(token string) (*MyClaims,int) {
-	setToken,_ := jwt.ParseWithClaims(token,&MyClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return JwtKey,nil
+func CheckToken(token string) (*MyClaims, int) {
+	setToken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
 	})
 
-	if key,_ := setToken.Claims.(*MyClaims); setToken.Valid {
-		return key,200
-	}else {
-		return nil,405
+	if key, _ := setToken.Claims.(*MyClaims); setToken.Valid {
+		return key, 200
+	} else {
+		return nil, 405
 	}
 
 }
+
 //jwt中间件
 // 全局验证器
-
-
 
 func JwtToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -62,10 +63,9 @@ func JwtToken() gin.HandlerFunc {
 		//非登录路劲进行token验证
 		if strings.Index(path, "login") < 0 {
 
-
 			token := c.Request.Header.Get("token")
 			token = c.Request.Header.Get("token")
-			fmt.Println("token为："+token)
+			fmt.Println("token为：" + token)
 			if token == "" {
 				c.JSON(
 					http.StatusOK,
@@ -82,7 +82,7 @@ func JwtToken() gin.HandlerFunc {
 			token = token[1:]
 			key, tCode := CheckToken(token)
 
-			if tCode == 405{
+			if tCode == 405 {
 
 				c.JSON(http.StatusOK,
 					gin.H{
@@ -105,20 +105,22 @@ func JwtToken() gin.HandlerFunc {
 				c.Abort()
 				return
 			}
-			c.Set("username",key.Username)
+			c.Set("username", key.Username)
+			c.Set("user_id", key.UserId)
 			c.Next()
 		}
 	}
 
 }
+
 //局部验证器 检验用户权限，普通用户无法访问admin后台
 func CheckAdminAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//fmt.Println("checkAdminAuth。。。。。。。。。。。。。。。。")
 		token := c.Request.Header.Get("token")
-		state:=token[0]-'0'
-		token=token[1:]
-		if state==2{
+		state := token[0] - '0'
+		token = token[1:]
+		if state == 2 {
 			c.JSON(http.StatusOK,
 				gin.H{
 					"success": false,
